f527890a4193c7d9afb48c5338e832b282eb98f9
Guigalaverna <guigalaverna@gmail.com>
2021-05-31 10:50:44 -0300

    User entity

---
+++ SmartDoorman/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  privateKey: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid()
+    }
+  }
+} 
+
+export { User }
