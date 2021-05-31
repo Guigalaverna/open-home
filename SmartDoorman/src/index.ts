bf63d7755e5672ebe207cf5bd23273169152ad94
Guigalaverna <guigalaverna@gmail.com>
2021-05-31 10:52:01 -0300

    Using JSON in body and create some routes

--- SmartDoorman/src/index.ts
+++ SmartDoorman/src/index.ts
@@ -9,6 +9,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3333
 
+app.use(express.json())
 app.use(router)
 
 app.listen(PORT, () => console.log('\n > Express running'))
--- SmartDoorman/src/routes.ts
+++ SmartDoorman/src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
+import PrivateKey from './controllers/PrivateKey'
 
 const app = Router()
 
-app.post('/api/virtual-key')
+app.post('/api/login', PrivateKey.login)
+app.post('/api/create-user', PrivateKey.create)
 
 export default app
