d89088eaf25b8d4cc956dd6d21a889a453ec2f3f
Guigalaverna <guigalaverna@gmail.com>
2021-05-31 02:07:14 -0300

    Add TypeORM and connecting into database

--- SmartDoorman/src/index.ts
+++ SmartDoorman/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import router from './routes'
+import './database'
 
 dotenv.config()
 
 const app = express();
 
 const PORT = process.env.PORT || 3333
+
+app.use(router)
 
 app.listen(PORT, () => console.log('\n > Express running'))
